bc25506d7deed9de1a10a9bca6ca93c73085cde9
rina harahap <rinaharahap33@gmail.com>
2024-02-29 16:03:27 +0700

    Prakweb2

---
+++ form.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container">
+    <form method="POST" action="hasil.php">
+  <div class="form-group row">
+    <label for="nama" class="col-4 col-form-label">Nama</label> 
+    <div class="col-8">
+      <input id="nama" name="nama" placeholder="Masukkan Nama Anda" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="matkul" class="col-4 col-form-label">Mata kuliah</label> 
+    <div class="col-8">
+      <select id="matkul" name="matkul" class="custom-select">
+        <option value="DDP">Dasar-dasar pemrograman</option>
+        <option value="Pemwep">pemrograman web</option>
+        <option value="Basdat">Basis data</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="uts" class="col-4 col-form-label">Nilai UTS</label> 
+    <div class="col-8">
+      <input id="uts" name="uts" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="uas" class="col-4 col-form-label">Nilai UAS</label> 
+    <div class="col-8">
+      <input id="uas" name="uas" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="praktikum" class="col-4 col-form-label">Nilai Praktikum</label> 
+    <div class="col-8">
+      <input id="praktikum" name="praktikum" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" value="data disimpan" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+</div>
+</body>
+</html>
+
+?>
